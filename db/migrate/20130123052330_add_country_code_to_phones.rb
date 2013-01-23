c4a6cfa1b5c85122159d55244880a6e3e5701b90
Andrew Narkewicz <anark6@gmail.com>
2013-01-22 22:56:29 -0800

    Update existion phone numbers to be canadian numbers

--- db/migrate/20130123052330_add_country_code_to_phones.rb
+++ db/migrate/20130123052330_add_country_code_to_phones.rb
@@ -1,5 +1,10 @@
 class AddCountryCodeToPhones < ActiveRecord::Migration
   def change
     add_column :phones, :country_code, :string, :default => "CA"
+
+    Phone.all.each do |phone|
+      current_number = phone.read_attribute :number
+      phone.update_attribute :number, "1#{current_number}"
+    end
   end
 end
